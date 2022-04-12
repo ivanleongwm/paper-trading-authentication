7797b1a6efa6e37261eb22823cd7f15630f47e0a
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-12 16:07:40 +0800

    purchase and sales schema and controller created

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const UserController = require("./controllers/userController")
+const PurchaseController = require ("./controllers/purchaseController")
 const methodOverride = require("method-override");
 const session = require("express-session");
 
@@ -36,6 +37,7 @@ app.use(methodOverride("_method"));
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", UserController);
+app.use("/api/purchase", PurchaseController);
 
 
 app.get("/", (req, res) => {
