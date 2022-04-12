a5b456ed9e313e9b8e5fce8f905d67f541f54b3e
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-12 16:12:23 +0800

    sales controller and sales log updated

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -3,17 +3,17 @@
 // =======================================
 const express = require("express");
 const { find, create } = require("../models/purchaseLog");
-const PurchaseLog = require("../models/purchaseLog");
+const SalesLog = require("../models/salesLog");
 const router = express.Router();
 
 router.get("/seed", async (req, res) => {
-  const purchaseDetails = [
-    { date: "2021-01-22", ticker: "AAPL", quantity: 3, purchasePrice: 5 },
-    { date: "2021-01-28", ticker: "GOOGL", quantity: 25, purchasePrice: 51 },
+  const salesDetails = [
+    { date: "2021-02-22", ticker: "AAPL", quantity: 3, salesPrice: 15 },
+    { date: "2021-02-28", ticker: "GOOGL", quantity: 25, salesPrice: 100 },
   ];
-  await PurchaseLog.deleteMany({});
-  await PurchaseLog.insertMany(purchaseDetails);
-  res.json(purchaseDetails);
+  await SalesLog.deleteMany({});
+  await SalesLog.insertMany(salesDetails);
+  res.json(salesDetails);
 });
 
 // =======================================
@@ -22,8 +22,8 @@ router.get("/seed", async (req, res) => {
 //Index route
 router.get("/", (req, res) => {
   User.find()
-    .then((purchaseDetails) => {
-      res.json(purchaseDetails);
+    .then((salesDetails) => {
+      res.json(salesDetails);
     })
     .catch((err) => {
       res.json(err);
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const UserController = require("./controllers/userController")
 const PurchaseController = require ("./controllers/purchaseController")
+const SalesController = require ("./controllers/salesController")
 const methodOverride = require("method-override");
 const session = require("express-session");
 
@@ -38,6 +39,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", UserController);
 app.use("/api/purchase", PurchaseController);
+app.use("/api/sales", SalesController);
 
 
 app.get("/", (req, res) => {
