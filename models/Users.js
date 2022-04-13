57099524eb6a1874a5ee2703b62545e1debdcaab
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-04-13 15:36:05 +0800

    Users.js updated

--- models/Users.js
+++ models/Users.js
@@ -14,6 +14,6 @@ const userSchema = new Schema({
     // newToTrading: {type: String, required: false },
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("account-details", userSchema);
 
 module.exports = User;
