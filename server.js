00da2696b520cafadb709da1e1a182b39c9441fb
ivanleongwm <ivanleongwm@gmail.com>
2022-04-16 10:43:53 +0800

    cors edited

--- server.js
+++ server.js
@@ -51,9 +51,6 @@ app.use(
   })
 );
 
-app.use(express.urlencoded({extended:false}));
-app.use(methodOverride("_method"));
-app.use(cors());
 app.use(express.json());
 app.use("/api/users", UserController);
 app.use("/api/purchase", PurchaseController);
